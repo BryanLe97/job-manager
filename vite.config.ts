bec482bc259983e24d85ff726b186aaa984c646c
Tuan Le <leminhtuan169@gmail.com>
2024-04-07 20:18:24 +0700

    feat(core): add gitignore

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,12 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-import { resolve,dirname } from 'path';
+import { resolve, dirname } from 'path';
 import { fileURLToPath } from 'url';
 
 const __dirname = dirname(fileURLToPath(import.meta.url));
 
 export default defineConfig({
-  base: 'job-manager/',
+  base: '/job-manager/',
   plugins: [vue()],
   server: {
     port: 8080,
